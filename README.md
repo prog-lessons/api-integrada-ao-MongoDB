6c61a4f0c2219189a88b50807480e965be1bb14c
cadu-tech <caduolivieri1984@gmail.com>
2021-01-05 15:11:28 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,37 @@
+##### Construindo um projeto de uma API.NET integrada ao MongoDB
+
+Desenvolvido no Bootcamp Avanade da Digital Innovation One
+
+### Ambiente:
+
+- Visual Studio 2019 
+- Mongo DB Atlas
+
+### Teste
+
+https://localhost:5001/infectado
+
+```
+{
+	 " dataNascimento " : " 1990-03-01 " ,
+	 " sexo " : " M " ,
+	 " latitude " : -23,5630994 ,
+	 " longitude " : -46,6565712 
+}
+```
+
+### Configurações
+
+Acesso ao banco no appsettings.
+
+------
+
+### Componentes
+
+https://docs.mongodb.com/
+
+https://docs.mongodb.com/manual/
+
+https://docs.mongodb.com/ecosystem/drivers/csharp/
+
+https://docs.atlas.mongodb.com/
---
+++ src/Controllers/InfectadoControllers.cs
@@ -0,0 +1,39 @@
+﻿using ProjetoAPI.Data.Collections;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using ProjetoAPI.Models;
+
+namespace ProjetoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+            return Ok(infectados);
+        }
+    }
+}
